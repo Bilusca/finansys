035f502348099b4fb4b8c132b4489a53504c0fbc
Lucas Assis Vieira <lucasav2005@hotmail.com>
2019-01-10 01:13:18 -0200

    Adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
-      { id: 1, name: 'Moradia', descriptio: 'Pagamentos de Contas da Casa' },
-      { id: 1, name: 'Saúde', descriptio: 'Plano de saúde e remédios' },
-      { id: 1, name: 'Lazer', descriptio: 'Cinema, parques, praia, etc' },
-      { id: 1, name: 'Salário', descriptio: 'Recebimento de salário' },
-      { id: 1, name: 'Freelas', descriptio: 'Trabalhos como freelancer' }
+    const categories: Category[] = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+      { id: 1, name: 'Saúde', description: 'Plano de saúde e remédios' },
+      { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 1, name: 'Salário', description: 'Recebimento de salário' },
+      { id: 1, name: 'Freelas', description: 'Trabalhos como freelancer' }
     ];
 
     return { categories };
